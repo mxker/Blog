bdd066d47a016307a9be1d2b60b1bc7da323aed2
maxikun <mxk_crazy@163com>
2019-12-03 17:09:36 +0800

    静态资源路由

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -89,8 +89,8 @@
 @yield('footer')
 
 <!-- Scripts -->
-<script src="{{ asset('blog/assets/js/jquery.min.js') }}"></script>
-<script src="{{ asset('blog/assets/js/skel.min.js') }}"></script>
+<script src="blog/assets/js/jquery.min.js"></script>
+<script src="blog/assets/js/skel.min.js"></script>
 <script src="{{ asset('blog/assets/js/util.js') }}"></script>
 {{--<!--[if lte IE 8]><script src="blog/assets/js/ie/respond.min.js"></script><![endif]-->--}}
 <script src="{{ asset('blog/assets/js/main.js') }}"></script>
